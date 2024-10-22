3726b592917ca07918acb8272ca157a43e6714fe
anastasiiiii <nastia262002@gmail.com>
2024-10-22 20:09:34 +0200

    add frame and snake class

---
+++ SnakeGame/src/main/java/org/example/SnakeGame.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import javax.swing.*;
+
+public class SnakeGame extends JPanel {
+    public SnakeGame(final int width, final int height) {
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

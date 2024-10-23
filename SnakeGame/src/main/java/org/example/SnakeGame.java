61180a2a0ec5063956725b43c508f0f0c487ec84
anastasiiiii <nastia262002@gmail.com>
2024-10-23 14:42:47 +0200

    create snake

--- SnakeGame/src/main/java/org/example/SnakeGame.java
+++ SnakeGame/src/main/java/org/example/SnakeGame.java
@@ -2,40 +2,118 @@ package org.example;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.font.TextLayout;
+import java.util.ArrayList;
 
-public class SnakeGame extends JPanel {
+public class SnakeGame extends JPanel implements ActionListener {
     private final int width;
     private final int height;
+    private final int cellSize;
+    private static final int FRAME_RATE = 20;
+    private boolean gameStarted = false;
+    private boolean gameOver = false;
+    private final ArrayList<GamePoint> snake = new ArrayList<>();
 
     public SnakeGame(final int width, final int height) {
         super();
         this.width = width;
         this.height = height;
+        this.cellSize = width / (FRAME_RATE * 2);
         setPreferredSize(new Dimension(width, height));
-        setBackground(Color.BLACK);
+        // RGB values for #00643a
+        int red = 0;
+        int green = 100;
+        int blue = 58;
+
+        // Convert RGB to HSB
+        float[] hsbValues = Color.RGBtoHSB(red, green, blue, null);
+
+        // Create Color object using HSB values
+        Color backgroundColor = Color.getHSBColor(hsbValues[0], hsbValues[1], hsbValues[2]);
+
+        setBackground(backgroundColor);
     }
 
     public void startGame() {
-        repaint();
+        resetGame();
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
+        requestFocusInWindow();
+        addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if(e.getKeyCode() == KeyEvent.VK_SPACE){
+                    gameStarted = true;
+                }
+            }
+        });
+
+        new Timer(1000 / FRAME_RATE, this).start();
     }
 
     @Override
     protected void paintComponent(Graphics graphics) {
         super.paintComponent(graphics);
-        graphics.setColor(Color.green);
-        graphics.setFont(graphics.getFont().deriveFont(30F));
-        int currentHeight = height / 4;
-        final var graphics2D = (Graphics2D) graphics;
-        final var frc = graphics2D.getFontRenderContext();
-        final String message = "This is a\nmulti-line message";
-        for(final var line : message.split("\n")) {
-            final var layout = new TextLayout(line, graphics.getFont(), frc);
-            final var bounds = layout.getBounds();
-            final var targetWidth = (float) (width - bounds.getWidth()) / 2;
-            layout.draw(graphics2D, targetWidth, currentHeight);
-            currentHeight += graphics.getFontMetrics().getHeight();
+        if(!gameStarted) {
+            graphics.setColor(Color.ORANGE);
+            graphics.setFont(graphics.getFont().deriveFont(30F));
+            int currentHeight = height / 4;
+            final var graphics2D = (Graphics2D) graphics;
+            final var frc = graphics2D.getFontRenderContext();
+            final String message = "Press Space To\nStart The Game";
+            for (final var line : message.split("\n")) {
+                final var layout = new TextLayout(line, graphics.getFont(), frc);
+                final var bounds = layout.getBounds();
+                final var targetWidth = (float) (width - bounds.getWidth()) / 2;
+                layout.draw(graphics2D, targetWidth, currentHeight);
+                currentHeight += graphics.getFontMetrics().getHeight();
+            }
+        } else {
+            graphics.setColor(Color.MAGENTA);
+            for (final var point : snake){
+                graphics.fillRect(point.x, point.y, cellSize, cellSize);
+            }
         }
+    }
+
+    private void resetGame() {
+        snake.clear();
+        snake.add(new GamePoint(width / 2, height / 2));
+    }
+
+    private void move() {
+        final GamePoint currentHead = snake.getFirst();
+        final GamePoint newHead = new GamePoint(currentHead.x+cellSize, currentHead.y);
+        snake.addFirst(newHead);
+
+        if(checkCollision()){
+            gameOver = true;
+            snake.removeFirst();
+        } else {
+            snake.removeLast();
+        }
+    }
+
+    private boolean checkCollision() {
+        final GamePoint head = snake.getFirst();
+        final var invalidWidth = (head.x < 0) || (head.x >= width);
+        final var invalidHeight = (head.y < 0) || (head.y >= height);
+        return invalidWidth || invalidHeight;
+    }
+
+    @Override
+    public void actionPerformed(final ActionEvent e) {
+        if(gameStarted) {
+            move();
+        }
+        repaint();
+    }
+
+    private record GamePoint(int x, int y){
 
     }
 }
